206b06e215a7b7d9169567dc23afc6d3a3e3f43e
Chow, Tiger S <tiger.s.chow@accenture.com>
2020-03-25 09:32:00 +0800

    update product name

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.iOS(.v10)],
     products: [
         .library(
-            name: "AppAuth",
+            name: "AppAuth_iOS",
             targets: ["AppAuth_iOS"]),
     ],
     dependencies: [
